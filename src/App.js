3fcc4a510409ec8ca7203bdc626be3831579a647
YusufHass <judekal82@gmail.com>
2024-01-22 17:02:40 -0500

    Routing to the home,hourly and daily is added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
+import { useEffect } from "react";
 import { Forcast } from "./components/Forcast";
 import { LocalTimeAndLocations } from "./components/LocalTimeAndLocation";
 import Navigation from "./components/Navigation";
 import { SearchWithZipCode } from "./components/SearchWithZipCode";
 import { Title } from "./components/Title";
 import { WeatherDetails } from "./components/WeatherDetails";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <div
-        className="mx-auto max-w-screen-md my-6 mt-4 py-7 px-32 bg-gradient-to-br rounded-xl bg-violet-200 h-fit shadow-x shadow-gray-400
+    <Router>
+      <div>
+        <div
+          className="mx-auto max-w-screen-md my-6 mt-4 py-7 px-32 bg-gradient-to-br rounded-xl bg-violet-200 h-fit shadow-x shadow-gray-400
     flex flex-row w-3-4 justify-center spa gap-x-5"
-      >
-        <Title />
-        <SearchWithZipCode />
-        <Navigation />
+        >
+          <Title />
+          <SearchWithZipCode />
+          <Navigation />
+        </div>
+        <div className="mx-auto max-w-screen-md my-6 mt-4 py-7 px-32 bg-gradient-to-br rounded-xl bg-violet-400 h-fit shadow-x shadow-gray-400 ">
+          <div>
+            <LocalTimeAndLocations />
+          </div>
+          <div>{/* <WeatherDetails /> */}</div>
+          <div>
+            <Routes>
+              <Route path="/" element={<WeatherDetails />} />
+              <Route
+                path="/hourly"
+                element={<Forcast title="Hourly Forecast" />}
+              />
+              <Route
+                path="/daily"
+                element={<Forcast title="Daily Forecast" />}
+              />
+            </Routes>
+          </div>
+        </div>
       </div>
-      <div className="mx-auto max-w-screen-md my-6 mt-4 py-7 px-32 bg-gradient-to-br rounded-xl bg-violet-400 h-fit shadow-x shadow-gray-400 ">
-        <div>
-          <LocalTimeAndLocations />
-        </div>
-        <div>
-          <WeatherDetails />
-        </div>
-        <div>
-          <Forcast title="Hourly Forcast" />
-          <Forcast title="Daily Forcast" />
-        </div>
-      </div>
-    </div>
+    </Router>
   );
 }
 
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,21 +1,33 @@
 // Navigation.js
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { Fragment, useEffect } from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 
 const Navigation = () => {
+//   const navigate = useNavigate();
+//   useEffect(() => {
+//     navigate("/");
+//   }, [navigate]);
   return (
-    <div className="flex flex-row w-3/4 items-center justify-center spa gap-x-4 ">
-      <div className="flex justify-around">
-        <button className="text-red-500 text-lg font-medium hover:scale-125">
-          Hourly
-        </button>
+    <Fragment>
+      <div className="flex flex-row w-3/4 items-center justify-center spa gap-x-4 ">
+        <div className="flex justify-around">
+          <Link
+            to="/hourly"
+            className="text-red-500 text-lg font-medium hover:scale-125"
+          >
+            Hourly
+          </Link>
+        </div>
+        <div className="flex justify-around">
+          <Link
+            to="/daily"
+            className="text-red-500 text-lg font-medium hover:scale-125"
+          >
+            Daily
+          </Link>
+        </div>
       </div>
-      <div className="flex justify-around">
-        <button className="text-red-500 text-lg font-medium hover:scale-125">
-          Daily
-        </button>
-      </div>
-    </div>
+    </Fragment>
   );
 };
 
