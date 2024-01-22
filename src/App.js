11e91b2897f50897d96b6b0d26f22fedb9840e00
YusufHass <judekal82@gmail.com>
2024-01-21 21:45:28 -0500

    Added title and logo along with some heading styles

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Title } from './components/Title';
+
+function App() {
+  return (
+    <div className="mx-auto max-w-screen-md mt-4 py-12 px-32 bg-gradient-to-br rounded-xl bg-cyan-950 h-fit shadow-xl shadow-gray-400">
+    <Title/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Title.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './App.css';
+export const Title = () => {
+  return (
+    <div >
+      <h1 className="text-white text-2xl">Weather App</h1>
+    </div>
+  );
+};
